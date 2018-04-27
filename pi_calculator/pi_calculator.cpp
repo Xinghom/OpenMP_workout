f0aa64ffcdd726a7ab8ad0607899aec5fdae9558
Xinghom <Xinghom@users.noreply.github.com>
2018-04-26 18:32:24 -0700

    get inform

--- pi_calculator/pi_calculator.cpp
+++ pi_calculator/pi_calculator.cpp
@@ -2,14 +2,22 @@
 #include <omp.h>
 
 int main (int argc, char * argv[]) {
+    int nthread, tid;
     double x, pi, step, sum=0.0;
     int i;
     long num_steps = 1000000000; 
     step = 1./(double)num_steps;
     #pragma omp parallel for private(x) reduction(+:sum)
     for (i=0; i<num_steps; i++) {
+        tid = omp_get_thread_num();
+        std::cout << "This thread is NO." << tid << std::endl;
         x = (i + .5)*step;
         sum = sum + 1.0/(1.+ x*x);
+
+        if (tid == 0) {
+            nthread = omp_get_num_thread();
+            std::cout << "Total threads: " << nthread << std::endl;
+        }
     }
     pi = 4.0*sum*step;
     std::cout << pi << std::endl;
