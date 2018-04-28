38c282395ffd638eef94a1fae4ee636748633308
Xinghom <Xinghom@users.noreply.github.com>
2018-04-28 02:57:06 -0700

    huge mat square done with percent display

--- matrix_multiplication/matix_square.cpp
+++ matrix_multiplication/matix_square.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
 #define ROW_NUM 1600
 #define COL_NUM 1600
+#define RANDOM_NUM_MAX 10
 
 using namespace std;
 
@@ -27,9 +29,27 @@ typedef vector<Vec> Mat;
 //     return prod;
 // }
 
+void printM(Mat m){
+    if (m.empty()) {
+        throw "Matrix is empty >.<";
+    }
+    int row = m.size();
+    int col = m[0].size();
+    
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << "--\n";
+    }
+}
+
+
 int main(){
     int m = ROW_NUM, n = COL_NUM;
     // Allocate space for the vectors
+    // If we initialize the vector length in this way, we can then assign individual elements directly, as in row[j] =
+    // rather than with push_back.
     Vec row(n);
     Mat matrix;
     srand(time(0));
@@ -37,15 +57,32 @@ int main(){
     cout << "Matrix generating...\n";
     for(int i = 0; i < m; i++){
         for(int j = 0; j < n; j++) {
-            row[j] = rand() % 100;
+            row[j] = rand() % RANDOM_NUM_MAX;
         }
-        // if (i <= 5) {
-        //     for(int k = 0; k <10; k++)
-        //         cout << row[k] << endl;
-        // }
         matrix.push_back(row);
     }
     cout << "Matrix Ready" << endl;
+
+    cout << "Matrix Squaring ..." << endl;
+    Mat result;
+    for (int row_1 = 0; row_1 < m; row_1++) {
+        for(int col = 0; col < n; col++) {
+            double sum;
+            for (int row_2 = 0; row_2 < m; row_2++) {
+                sum += matrix[row_1][row_2] * matrix[row_2][col];
+            }
+            row[col] = sum;
+        }
+        result.push_back(row);
+        cout << to_string(row_1) + "/" + to_string(m) << endl;
+    }
+    cout << "square done." << endl;
+    try {
+        printM(result);
+    } catch (char * msg) {
+        cout << msg << endl;
+    }
     
+    return 0;
 }
 
