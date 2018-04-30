0222755a5a820aa627632e30ef630310f7cac612
Xinghom <27728775+Xinghom@users.noreply.github.com>
2018-04-29 21:34:07 -0700

    Still debugging, but find problem could be "for loop" cannot be excute in master region

--- matrix_multiplication/Mat_Sqr_outerLoop.cpp
+++ matrix_multiplication/Mat_Sqr_outerLoop.cpp
@@ -32,7 +32,7 @@ void printM(Mat m){
 
 
 int main(){
-    int m = ROW_NUM, n = COL_NUM;
+    int m = ROW_NUM, n = COL_NUM, i, j, row_1, col, row_2;
     int tID, numThreads, chunk = 10;
     // Allocate space for the vectors
     // If we initialize the vector length in this way, we can then assign individual elements directly, as in row[j] =
@@ -43,7 +43,7 @@ int main(){
     srand(time(0));
 
     #pragma omp parallel shared(matrix, result, row, numThreads, chunk) \
-                        private(m,n,tID)
+                        private(m,n,tID,i,j,row_1,col,row_2)
     {
         tID = omp_get_thread_num();
         if (tID == 0) {
@@ -51,18 +51,24 @@ int main(){
             cout << "Total Threads Number: " << numThreads << endl;
         }
         #pragma omp barrier
-        #pragma omp single
+        #pragma omp master
         {
             cout << "I'm worker thread NO." << tID << endl;
             cout << "Matrix generating...\n";
-            for(int i = 0; i < m; i++){
-                for(int j = 0; j < n; j++) {
+            for(i = 0; i < m; i++){
+                cout << "test 1 outer" << endl;
+                cout << "var i = " << i << endl;
+                for(j = 0; j < n; j++) {
                     row[j] = rand() % RANDOM_NUM_MAX + RANDOM_NUM_LOWER_BOUND;
-                }
+               	    cout << "test 1" << endl;
+		    cout << "var j = " << j << endl;
+ 		    if (!row.empty()) cout << row[j] << endl;
+		    else cout << "row vector is empty in for loop" << endl; 
+		 }
                 matrix.push_back(row);
             }
-            for(int i = 0; i < m; i++){
-                for(int j = 0; j < n; j++) {
+            for(i = 0; i < m; i++){
+                for(j = 0; j < n; j++) {
                     row[j] = 0;
                 }
                 result.push_back(row);
@@ -70,13 +76,32 @@ int main(){
             cout << "Matrix Ready" << endl;
             cout << "Again, I'm worker thread NO." << tID << endl;
             cout << "Matrix Squaring ..." << endl;
+        try {
+            printM(matrix);
+            cout << "=================" << endl;
+            printM(result);
+            cout << "================" << endl;
+        } catch (char const * msg) {
+            cout << msg << endl;
         }
+        }
+	#pragma omp single
+{
+	try {
+	    printM(matrix);
+	    cout << "=================" << endl;
+	    printM(result);
+            cout << "================" << endl;
+	} catch (char const * msg) {
+	    cout << msg << endl;
+	}
+}
         // cout << "Thread No." << tID << " starts Multiplying!" << endl;
         #pragma omp for schedule (static, chunk)
-        for (int row_1 = 0; row_1 < m; row_1++) {
+        for (row_1 = 0; row_1 < m; row_1++) {
             // cout << "thread " << tID << " is doing row " << row_1 << endl;
-            for (int col = 0; col < n; col++) {
-                for (int row_2 = 0; row_2 < m; row_2++) {
+            for (col = 0; col < n; col++) {
+                for (row_2 = 0; row_2 < m; row_2++) {
                     result[row_1][col] += matrix[row_1][row_2] * matrix[row_2][col];
                 }
             }
