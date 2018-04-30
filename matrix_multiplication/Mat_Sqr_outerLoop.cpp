62e80090b12a854c83317253a46debf611a24b19
Xinghom <Xinghom@users.noreply.github.com>
2018-04-29 22:05:17 -0700

    debug sol v3 done, let's test it on Cori

--- matrix_multiplication/Mat_Sqr_outerLoop.cpp
+++ matrix_multiplication/Mat_Sqr_outerLoop.cpp
@@ -41,6 +41,38 @@ int main(){
     Mat matrix;
     Mat result;
     srand(time(0));
+ 
+    for(i = 0; i < m; i++){
+        cout << "test 1 outer" << endl;
+        cout << "var i = " << i << endl;
+        for(j = 0; j < n; j++) {
+            row[j] = rand() % RANDOM_NUM_MAX + RANDOM_NUM_LOWER_BOUND;
+            cout << "test 1" << endl;
+            cout << "var j = " << j << endl;
+            if (!row.empty()) cout << row[j] << endl;
+            else cout << "row vector is empty in for loop" << endl; 
+        }
+        matrix.push_back(row);
+    }
+
+    for(i = 0; i < m; i++){
+        for(j = 0; j < n; j++) {
+            row[j] = 0;
+        }
+        result.push_back(row);
+    }
+
+    cout << "Matrix Ready" << endl;
+    cout << "Again, I'm worker thread NO." << tID << endl;
+    cout << "Matrix Squaring ..." << endl;
+    try {
+        printM(matrix);
+        cout << "=================" << endl;
+        printM(result);
+        cout << "================" << endl;
+    } catch (char const * msg) {
+        cout << msg << endl;
+    }
 
     #pragma omp parallel shared(matrix, result, row, numThreads, chunk) private(m,n,tID,i,j,row_1,col,row_2)
     {
@@ -48,43 +80,7 @@ int main(){
         if (tID == 0) {
             numThreads = omp_get_num_threads();
             cout << "Total Threads Number: " << numThreads << endl;
-	    cout << "Matrix generating...\n";
         }
-            
-        #pragma omp for schedule (static, chunk)
-            for(i = 0; i < m; i++){
-                cout << "test 1 outer" << endl;
-                cout << "var i = " << i << endl;
-                for(j = 0; j < n; j++) {
-                    row[j] = rand() % RANDOM_NUM_MAX + RANDOM_NUM_LOWER_BOUND;
-               	    cout << "test 1" << endl;
-                    cout << "var j = " << j << endl;
-                    if (!row.empty()) cout << row[j] << endl;
-                    else cout << "row vector is empty in for loop" << endl; 
-		        }
-                matrix.push_back(row);
-            }
-        #pragma omp for schedule (static, chunk)
-            for(i = 0; i < m; i++){
-                for(j = 0; j < n; j++) {
-                    row[j] = 0;
-                }
-                result.push_back(row);
-            }
-        #pragma omp single
-        {
-            cout << "Matrix Ready" << endl;
-            cout << "Again, I'm worker thread NO." << tID << endl;
-            cout << "Matrix Squaring ..." << endl;
-            try {
-                printM(matrix);
-                cout << "=================" << endl;
-                printM(result);
-                cout << "================" << endl;
-            } catch (char const * msg) {
-                cout << msg << endl;
-            }
-	}
         #pragma omp for schedule (static, chunk)
         for (row_1 = 0; row_1 < m; row_1++) {
             cout << "thread " << tID << " is doing row " << row_1 << endl;
