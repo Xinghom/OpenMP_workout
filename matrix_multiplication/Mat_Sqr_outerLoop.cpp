28faec2d7625d3932776a70a5f2cf8f235ed8793
Xinghom <27728775+Xinghom@users.noreply.github.com>
2018-04-29 22:26:28 -0700

    Finallygit add Mat_Sqr_outerLoop.cpp! Fixedgit add Mat_Sqr_outerLoop.cpp! It worksgit add Mat_Sqr_outerLoop.cpp! Super Fastgit add Mat_Sqr_outerLoop.cppgit add Mat_Sqr_outerLoop.cpp Oh yeahgit add Mat_Sqr_outerLoop.cppgit add Mat_Sqr_outerLoop.cppgit add Mat_Sqr_outerLoop.cpp

--- matrix_multiplication/Mat_Sqr_outerLoop.cpp
+++ matrix_multiplication/Mat_Sqr_outerLoop.cpp
@@ -5,9 +5,9 @@
 #include <time.h>       /* time */
 #include <omp.h>
 
-#define ROW_NUM 11
-#define COL_NUM 11
-#define RANDOM_NUM_MAX 5
+#define ROW_NUM 1600
+#define COL_NUM 1600
+#define RANDOM_NUM_MAX 100
 #define RANDOM_NUM_LOWER_BOUND 1
 
 using namespace std;
@@ -43,14 +43,8 @@ int main(){
     srand(time(0));
  
     for(i = 0; i < m; i++){
-        cout << "test 1 outer" << endl;
-        cout << "var i = " << i << endl;
         for(j = 0; j < n; j++) {
             row[j] = rand() % RANDOM_NUM_MAX + RANDOM_NUM_LOWER_BOUND;
-            cout << "test 1" << endl;
-            cout << "var j = " << j << endl;
-            if (!row.empty()) cout << row[j] << endl;
-            else cout << "row vector is empty in for loop" << endl; 
         }
         matrix.push_back(row);
     }
@@ -63,18 +57,9 @@ int main(){
     }
 
     cout << "Matrix Ready" << endl;
-    cout << "Again, I'm worker thread NO." << tID << endl;
     cout << "Matrix Squaring ..." << endl;
-    try {
-        printM(matrix);
-        cout << "=================" << endl;
-        printM(result);
-        cout << "================" << endl;
-    } catch (char const * msg) {
-        cout << msg << endl;
-    }
 
-    #pragma omp parallel shared(matrix, result, row, numThreads, chunk) private(m,n,tID,i,j,row_1,col,row_2)
+    #pragma omp parallel shared (matrix, result, numThreads, chunk) private(tID,row_1,col,row_2)
     {
         tID = omp_get_thread_num();
         if (tID == 0) {
@@ -82,23 +67,21 @@ int main(){
             cout << "Total Threads Number: " << numThreads << endl;
         }
         #pragma omp for schedule (static, chunk)
-        for (row_1 = 0; row_1 < m; row_1++) {
+        for (row_1 = 0; row_1 < ROW_NUM; row_1++) {
             cout << "thread " << tID << " is doing row " << row_1 << endl;
-            for (col = 0; col < n; col++) {
-                for (row_2 = 0; row_2 < m; row_2++) {
+            for (col = 0; col < COL_NUM; col++) {
+                for (row_2 = 0; row_2 < ROW_NUM; row_2++) {
                     result[row_1][col] += matrix[row_1][row_2] * matrix[row_2][col];
                 }
             }
         }
     }
     cout << "Square done." << endl;
-    cout << "size m: " << result.size() << endl;
-    // cout << result[0][0] << endl;
-    try {
-        printM(result);
-    } catch (char const* msg) {
-        cout << msg << endl;
-    }
+//    try {
+//        printM(result);
+//    } catch (char const* msg) {
+//        cout << msg << endl;
+//    }
     return 0;
 }
 
