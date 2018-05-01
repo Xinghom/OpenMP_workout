51ca8d516cc65a13217822a229258903bcd2d3ad
Xinghom <Xinghom@users.noreply.github.com>
2018-04-30 20:43:16 -0700

    stable version and clean

--- pi_calculator/pi_calculator.cpp
+++ pi_calculator/pi_calculator.cpp
@@ -12,14 +12,8 @@ int main (int argc, char * argv[]) {
     auto start = std::chrono::steady_clock::now( );
     #pragma omp parallel for private(x) reduction(+:sum)
     for (i=0; i<num_steps; i++) {
-//        tid = omp_get_thread_num();
-//        std::cout << "This thread is NO." << tid << std::endl;
         x = (i + .001)*step;
         sum = sum + 1.0/(1.+ x*x);
-//        if (tid == 0) {
-//            nthread = omp_get_num_threads();
-//            std::cout << "Total threads: " << nthread << std::endl;
-//        }
     }
     pi = 4.0*sum*step;
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::steady_clock::now( ) - start );
